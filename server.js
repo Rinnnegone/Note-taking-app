40e147209df6e5e1213b983ad6b666b3c531bef3
luis <bluerizabiz@gmail.com>
2022-04-18 12:12:35 -0500

    still needs middelware

--- server.js
+++ server.js
@@ -5,3 +5,16 @@ const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 const app = express();
 const path = require('path');
+
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use('api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+})
